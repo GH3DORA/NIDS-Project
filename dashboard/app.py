de7efbf6a6d0ee25e959c6a15af61f579731382d
GH3DORA <chubbysehaj@gmail.com>
2026-02-24 00:34:10 +0530

    created flask file

---
+++ dashboard/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, jsonify
+from parser import get_dashboard_data
+
+app=Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api/data")
+def api_data():
+    data=get_dashboard_data()
+    return jsonify(data)
+
+if __name__=="main":
+    app.run(debug=True,port=5000)
